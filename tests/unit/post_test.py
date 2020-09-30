198821f969c448ea3c218459f1be9acc1d882a12
Jhon Catacora <_jhon@localhost.localdomain>
2020-09-30 15:24:58 -0500

    update last commit

--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -10,4 +10,4 @@ class PostTest(TestCase):
     def test_json(self):
         p = Post('Test', 'Test Content')
         expected = { 'title': 'Test', 'content': 'Test Content'}
-        self.assertDictEqual(expected, p.json())
+        self.assertDictEqual(expected, p.json())        
